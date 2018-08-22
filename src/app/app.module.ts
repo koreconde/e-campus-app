56813ec142172a05c77058066c537ad1d85badaa
Remyce Savanna <remycesavanna@gmail.com>
2018-08-22 14:32:50 +0100

    did deploy setup and added packges

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, ErrorHandler } from '@angular/core';
+import { NgModule, ErrorHandler, Injectable, Injector } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -105,7 +105,37 @@ import { VgCoreModule } from 'videogular2/core';
 import { VgControlsModule } from 'videogular2/controls';
 import { VgOverlayPlayModule } from 'videogular2/overlay-play';
 
-enableProdMode();
+// These are all imports required for Pro Client with Monitoring & Deploy,
+// feel free to merge into existing imports above.
+import { Pro } from '@ionic/pro';
+//import { ErrorHandler, Injectable, Injector } from '@angular/core';
+//import { IonicErrorHandler } from 'ionic-angular';
+
+Pro.init('A0567B07', {
+  appVersion: '0.0.1'
+})
+
+@Injectable()
+export class MyErrorHandler implements ErrorHandler {
+  ionicErrorHandler: IonicErrorHandler;
+
+  constructor(injector: Injector) {
+    try {
+      this.ionicErrorHandler = injector.get(IonicErrorHandler);
+    } catch(e) {
+      // Unable to get the IonicErrorHandler provider, ensure
+      // IonicErrorHandler has been added to the providers list below
+    }
+  }
+
+  handleError(err: any): void {
+    Pro.monitoring.handleNewError(err);
+    // Remove this if you want to disable Ionic's auto exception handling
+    // in development mode.
+    this.ionicErrorHandler && this.ionicErrorHandler.handleError(err);
+  }
+}
+
 @NgModule({
   declarations: [
     MyApp,
@@ -216,7 +246,10 @@ enableProdMode();
   WishlistService,
   WalletService,
   ImgcacheService,
-  BlogService
+  BlogService,
+  IonicErrorHandler,
+  [{ provide: ErrorHandler, useClass: MyErrorHandler }]
   ]
 })
+
 export class AppModule {}
